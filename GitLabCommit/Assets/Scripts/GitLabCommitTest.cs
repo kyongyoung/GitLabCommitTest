f49b217dcb320421bf443ca698047d014eeec2d4
Taily <taily9621@gmail.com>
2022-04-06 18:34:52 +0900

    1. Scripts 폴더 생성 2. GitLabCommitTest.cs 파일 생성

---
+++ GitLabCommit/Assets/Scripts/GitLabCommitTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitLabCommitTest : MonoBehaviour
+{
+    // 작성자 : 김공무 매니저
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
