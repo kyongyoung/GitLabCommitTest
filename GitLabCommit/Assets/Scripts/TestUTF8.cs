c253954b7cb1a4d0ae623f69ecb2882b36348f7a
Taily <taily9621@gmail.com>
2022-04-08 15:09:59 +0900

    1. UTF-8 타입 적용 테스트 스크립트 추가

---
+++ GitLabCommit/Assets/Scripts/TestUTF8.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUTF8 : MonoBehaviour
+{
+    // UTF-8 타입 적용 테스트
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("UTF-8 타입으로 변경 적용 테스트");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
